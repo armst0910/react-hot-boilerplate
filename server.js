c1265c511176d351d4fdf514c25ca20de0c95656
กัมปนาท เก่งพรสกุล < >
2016-06-17 16:56:00 +0700

    replace webpack-dev-server with express.js and add example from redux official website etc.

--- server.js
+++ server.js
@@ -1,15 +1,34 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+// var webpack = require('webpack');
+// var WebpackDevServer = require('webpack-dev-server');
+// var config = require('./webpack.config');
+//
+// new WebpackDevServer(webpack(config), {
+//   publicPath: config.output.publicPath,
+//   hot: true,
+//   historyApiFallback: true
+// }).listen(3000, 'localhost', function (err, result) {
+//   if (err) {
+//     return console.log(err);
+//   }
+//
+//   console.log('Listening at http://localhost:3000/');
+// });
 
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true
-}).listen(3000, 'localhost', function (err, result) {
-  if (err) {
-    return console.log(err);
-  }
+import Express from 'express';
+import BodyParser from 'body-parser';
 
-  console.log('Listening at http://localhost:3000/');
+import webpack from 'webpack';
+import config from './webpack.config';
+import webpackDevMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+
+const app = new Express();
+const compiler = webpack(config);
+app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath}));
+app.use(webpackHotMiddleware(compiler));
+
+app.get('/*', function(req, res){
+  res.sendFile(__dirname+'/index.html');
 });
+
+app.listen('3000');
